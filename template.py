747a675aad55f2746c61a72f51ffa3e565fb3d59
Sriram R <sriramr244@gmail.com>
2024-01-25 13:03:25 -0500

    Changed the project structure

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s")
-project_name = "cnnClassifier"
+project_name = "Sentence_similarity"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
